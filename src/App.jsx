56826a6a071012337a67433aa5212d40e07e00a3
AuraSync <AuraSync@VIT-SYS-952.wdc.vidyalankarlive.com>
2026-03-18 18:04:53 +0530

    feat: add routing with black homepage and Netlify config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import BlackPage from "./pages/BlackPage";
 
 const App = () => {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Routes>
+      <Route path="/" element={<BlackPage />} />
+      <Route path="/gamejam" element={<HomePage />} />
+    </Routes>
   );
 };
 
---
+++ src/pages/BlackPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BlackPage = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#000000",
+        margin: 0,
+        padding: 0,
+      }}
+    />
+  );
+};
+
+export default BlackPage;
