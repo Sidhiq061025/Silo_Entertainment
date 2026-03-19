a5321c3400697fc4d8741cbfff3a6ee9e67ff5fa
Sidhiq061025 <130834092+Sidhiq061025@users.noreply.github.com>
2026-03-19 21:56:23 +0530

    Update Description.jsx

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -7,7 +7,19 @@ export default function Description() {
         replayable games within a limited time. The event focuses on creating
         games that are simple to understand, addictive to play, and exciting to
         share or stream online. Many successful indie games started from small
-        prototypes like these, turning simple ideas into viral experiences.
+        prototypes like these, turning simple ideas into viral experiences.{" "}
+        
+        <br /><br />
+
+        👉{" "}
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLScntRpWRnT7l-pST7Lk7pjblLqdH2vkhRqaWRdQzKiGQqbCuw/viewform?usp=dialog"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="register-link"
+        >
+          Register your team here
+        </a>
       </p>
     </section>
   );
